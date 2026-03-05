7891735a6fec864ebab5596b22d39db27686f4a3
Thiago Castro <nascimentocastro989@gmail.com>
2026-03-05 12:16:47 -0300

    metodo de buscar notificacoes por id adicionado

--- src/main/java/org/thgcastro/agendamentonotificacaoapi/business/AgendamentoService.java
+++ src/main/java/org/thgcastro/agendamentonotificacaoapi/business/AgendamentoService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.thgcastro.agendamentonotificacaoapi.business.mapper.IAgendamentoMapper;
 import org.thgcastro.agendamentonotificacaoapi.controller.dto.in.AgendamentoRecord;
 import org.thgcastro.agendamentonotificacaoapi.controller.dto.out.AgendamentoRecordOut;
+import org.thgcastro.agendamentonotificacaoapi.infrastructure.exception.NotFoundException;
 import org.thgcastro.agendamentonotificacaoapi.infrastructure.repositories.AgendamentoRepository;
 
 @Service
@@ -19,5 +20,8 @@ public class AgendamentoService {
         return agendamentoMapper.paraOut(repository.save(agendamentoMapper.paraEntity(agendamento)));
     }
 
-
+    public AgendamentoRecordOut buscarAgendamentosPorId(Long id){
+        return agendamentoMapper.paraOut(repository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Id não encontrado")));
+    }
 }
