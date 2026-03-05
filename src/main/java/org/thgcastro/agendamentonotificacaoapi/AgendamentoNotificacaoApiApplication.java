b05367eb837909ba1bf04315c60a0e2fdb1cb997
Thiago Castro <nascimentocastro989@gmail.com>
2026-03-05 10:20:30 -0300

    commit inicial

---
+++ src/main/java/org/thgcastro/agendamentonotificacaoapi/AgendamentoNotificacaoApiApplication.java
@@ -0,0 +1,13 @@
+package org.thgcastro.agendamentonotificacaoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoNotificacaoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgendamentoNotificacaoApiApplication.class, args);
+    }
+
+}
